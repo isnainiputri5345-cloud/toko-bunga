2fd45af203a98dce21c9b5753e926de805b36e84
isnainiputri5345 <isnainiputri5345@gmail.com>
2026-07-01 10:47:20 +0800

    ini adalah lanjutan

--- admin/login.php
+++ admin/login.php
@@ -2,76 +2,48 @@
 session_start();
 include "../config/koneksi.php";
 
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
+    $username = mysqli_real_escape_string($koneksi, $_POST['username']);
+    $password = $_POST['password'];
 
-$username = $_POST['username'];
-$password = md5($_POST['password']);
+    $cek = mysqli_query($koneksi, "
+        SELECT * FROM admin
+        WHERE username='$username'
+        LIMIT 1
+    ");
 
-$cek = mysqli_query($koneksi,"
-SELECT * FROM admin
-WHERE username='$username'
-AND password='$password'
-");
+    if ($cek && mysqli_num_rows($cek) > 0) {
+        $data = mysqli_fetch_assoc($cek);
+        $passwordDb = $data['password'];
 
-if(mysqli_num_rows($cek)>0){
+        if ($passwordDb === md5($password) || $passwordDb === $password) {
+            $_SESSION['admin'] = true;
+            $_SESSION['admin_username'] = $data['username'];
 
-$_SESSION['admin']=true;
+            header("Location: dashboard.php");
+            exit;
+        }
+    }
 
-header("Location:dashboard.php");
-
-}else{
-
-echo "<script>
-alert('Login Gagal');
-</script>";
-
-}
+    echo "<script>alert('Login Gagal');</script>";
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-
 <title>Admin Login</title>
-
-<link rel="stylesheet"
-href="../assets/css/admin.css">
-
+<link rel="stylesheet" href="../assets/css/admin.css">
 </head>
-
 <body>
-
 <div class="login-admin">
-
 <h1>ERLISNA FLORIST</h1>
-
 <h3>Admin Login</h3>
-
 <form method="POST">
-
-<input
-type="text"
-name="username"
-placeholder="Username"
-required>
-
-<input
-type="password"
-name="password"
-placeholder="Password"
-required>
-
-<button
-name="login">
-
-Masuk
-
-</button>
-
+<input type="text" name="username" placeholder="Username" required>
+<input type="password" name="password" placeholder="Password" required>
+<button name="login">Masuk</button>
 </form>
-
 </div>
-
 </body>
 </html>
